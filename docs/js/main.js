185382365797a0b96df0faeb9779ad4d4d78baae
IlyaIvavov <85294343+IlyaIvavov@users.noreply.github.com>
2022-05-20 17:46:28 +0300

    Почти сделал 3ю секцию

--- docs/js/main.js
+++ docs/js/main.js
@@ -74,7 +74,17 @@ pageSlider.init();
 
 // Mobile Swipers
 
-let firstMobSlider = new Swiper('.swiper', {
+let firstMobSlider = new Swiper('.offer-mobile__slider', {
+	scrollbar: {
+		el: '.scrollbar',
+		dragClass: 'scrollbar-drag',
+		draggable: true,
+	},
+	grabCursor: true,
+	spaceBetween: 16,
+});
+
+let secondMobSlider = new Swiper('.projects-mobile-slider', {
 	scrollbar: {
 		el: '.scrollbar',
 		dragClass: 'scrollbar-drag',
